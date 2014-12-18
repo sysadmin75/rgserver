d639144288cc60d91d9ed0de76458be0df1fa860
Peter Wen <peter@whitehalmos.org>
2014-12-17 19:40:30 -0500

    Add dos2unix conversion for Windows vagrant.

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -15,6 +15,7 @@ pip install -r /rgserver/requirements.txt
 apt-get install -y postgresql
 apt-get install -y postgresql-client
 apt-get install -y nginx
+apt-get install -y dos2unix
 
 cat /rgserver/vagrant/pg_hba.conf > /etc/postgresql/9.4/main/pg_hba.conf
 /etc/init.d/postgresql restart
@@ -29,4 +30,5 @@ rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/rgserver.conf /etc/nginx/sites-enabled/rgserver.conf
 
 cp /rgserver/vagrant/rg /usr/bin/rg
+dos2unix /usr/bin/rg
 rg restart
